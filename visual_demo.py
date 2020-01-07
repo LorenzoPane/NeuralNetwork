4abdd4a75c2a941d7f884ae8e4e7e391aaa179a7
Lorenzo Pane <lorenzo@pane.net>
2020-01-07 12:26:38 -0500

    Rename gui.py

---
+++ visual_demo.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import NeuralNetwork as nn
+import numpy as np
+
+if __name__ == '__main__`:
+    testing_images, testing_labels = nn.load_data('mnist/test.csv')
+
+    network = nn.Network([784, 100, 10], .5)
+    network.weights = np.load('dump.npy', allow_pickle=True)
+
+    plt.gray()
+
+    for i in range(len(testing_images)):
+        image = testing_images[i]
+        for a, b in enumerate(["{:.2f}%".format(num*100) for num in network.apply_network(testing_images[i])[:, 0]], 0):
+            print(a, b)
+        plt.imshow(image.reshape(28, 28))
+        plt.show()
